1f68e9820fca3988ae26d93a2069cc29c549b007
aden kesuma <adenfdfd10@gmail.com>
2023-04-11 18:34:35 +0700

    add large other cities feature

--- src/components/random-place/RandomPlace.jsx
+++ src/components/random-place/RandomPlace.jsx
@@ -2,14 +2,36 @@ import React from "react";
 import "./RandomPlace.css";
 
 export default function RandomPlace({ shanghai, tokyo }) {
-    console.log(shanghai)
-    console.log(tokyo)
+    const {name, sys, weather} = shanghai;
+    const {nameT, sysT, weatherT} = tokyo;
+
     return (
-        <div className="map">
-            <p>{shanghai.name}, {shanghai.timezone}</p>
-            <p>{tokyo.name}, {shanghai.timezone}</p>
-            {/* <p>{data.weather[0].description}</p>
-            <img src={`http://openweathermap.org/img/wn/${item.weather[0].icon}.png`} alt="icon"/> */}
+        <div className="random-place">
+            {shanghai && <h3 className="heading-3">Other large cities</h3>}
+            {shanghai && (
+            <div className="random-place__box">
+                <p>
+                {name} {shanghai && ","} {sys.country}
+                </p>
+                <p>{weather[0].description}</p>
+                <img
+                src={`http://openweathermap.org/img/wn/${weather[0].icon}.png`}
+                alt="icon"
+                />
+            </div>
+            )}
+            {tokyo && (
+            <div className="random-place__box">
+                <p>
+                {tokyo.name} {tokyo && ","} {tokyo.timezone}
+                </p>
+                {/* <p>{data.weather[0].description}</p>
+                <img
+                src={`http://openweathermap.org/img/wn/${item.weather[0].icon}.png`}
+                alt="icon"
+                /> */}
+            </div>
+            )}
         </div>
         );
 }
